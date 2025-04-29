f89340be46fd2ba735a66f4671b1ed143b146b64
Your Name <gthabarbaer1@gmail.com>
2025-04-29 01:23:43 -0700

    ss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
